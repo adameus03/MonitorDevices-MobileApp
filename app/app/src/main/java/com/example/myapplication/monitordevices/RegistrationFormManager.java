60bf6f13c077c1c5c33fc885b9d848f144ec73c9
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-07 18:15:59 +0100

    add RegistrationFormManager

---
+++ app/app/src/main/java/com/example/myapplication/monitordevices/RegistrationFormManager.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.monitordevices;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationFormManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_register_form);
+    }
+}
