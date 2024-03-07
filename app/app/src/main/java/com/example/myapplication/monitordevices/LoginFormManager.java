60cb107ae3a3bf9eb914a8e7099c86363220b661
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-07 17:45:49 +0100

    add LoginFormManager

---
+++ app/app/src/main/java/com/example/myapplication/monitordevices/LoginFormManager.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.monitordevices;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginFormManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_login_form);
+    }
+}
