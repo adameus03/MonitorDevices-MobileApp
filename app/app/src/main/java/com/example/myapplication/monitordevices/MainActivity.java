2f124b0987543a7bef32f49a467c7f47462b024a
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-03-07 17:30:25 +0100

    handle main page activity buttons

--- app/app/src/main/java/com/example/myapplication/monitordevices/MainActivity.java
+++ app/app/src/main/java/com/example/myapplication/monitordevices/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication.monitordevices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openLoginForm(View view) {
+        Intent intent = new Intent(this, LoginFormManager.class);
+        startActivity(intent);
+    }
+
+    public void openRegistrationForm(View view) {
+        Intent intent = new Intent(this, RegistrationFormManager.class);
+        startActivity(intent);
+    }
+
 }
