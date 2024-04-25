2110743cbcc4745ff5ea28a58474a427834c99a7
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-25 08:55:00 +0200

    add ServerManager

--- app/app/src/main/java/com/example/myapplication/monitordevices/ServerManager.java
+++ app/app/src/main/java/com/example/myapplication/monitordevices/ServerManager.java
@@ -1,58 +1,13 @@
-package org.example;
-
-import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.apache.maven.project.builder.ProjectUri.MailingLists.MailingList.post;
+package com.example.myapplication.monitordevices;
 
 public class ServerManager {
-    public void addUser() {
-        URL url = new URL("http://localhost:8090/");
-        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-        conn.setReadTimeout(10000);
-        conn.setConnectTimeout(15000);
-        conn.setRequestMethod("POST");
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
+    private String REGISTER_URL = "http://192.168.100.4:8090/register/user";
 
-        List<String> params = new ArrayList<String>();
-        params.add("email: email132");
-        params.add("name: name1");
-        params.add("password: abc123");
-
-        OutputStream os = conn.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(os, "UTF-8"));
-        writer.write(getQuery(params));
-        writer.flush();
-        writer.close();
-        os.close();
-
-        conn.connect();
+    public void registerUser(String username, String password, String email) {
+        RegistrationManager registrationManager = new RegistrationManager(REGISTER_URL,
+                username, password, email);
+        registrationManager.registerUser();
     }
 
-    private String getQuery(List<String> params) throws UnsupportedEncodingException
-    {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
 
-        for (String pair : params)
-        {
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(pair, "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(pair, "UTF-8"));
-        }
-
-        return result.toString();
-    }
 }
