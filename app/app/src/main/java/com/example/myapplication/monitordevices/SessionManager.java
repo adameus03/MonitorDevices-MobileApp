c02f53d6843a3d9797c6d04fbc20cf84b7c844d7
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-10 20:32:55 +0200

    add session manager

--- app/app/src/main/java/com/example/myapplication/monitordevices/SessionManager.java
+++ app/app/src/main/java/com/example/myapplication/monitordevices/SessionManager.java
@@ -1,2 +1,26 @@
-package com.example.myapplication.monitordevices;public class SessionManager {
+package com.example.myapplication.monitordevices;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SessionManager {
+    private static final String PREF_NAME = "token";
+    private static final String KEY_TOKEN = "JWT";
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public SessionManager(Context context) {
+        this.sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        this.editor = sharedPreferences.edit();
+    }
+
+    public void saveToken(String token) {
+        editor.putString(KEY_TOKEN, token);
+        editor.commit();
+    }
+
+    public String getToken() {
+        return sharedPreferences.getString(KEY_TOKEN, null);
+    }
 }
