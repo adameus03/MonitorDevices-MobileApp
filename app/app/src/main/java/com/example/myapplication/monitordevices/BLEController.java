378bd789511f69aa11b7e73b5523a5cf82d1244e
mundus <mundus@pc6.home>
2024-07-05 16:45:57 +0200

    Can pass all required registration data (hardcoded for now) to the GATT server. Feedback notification on network check not handled properly yet

--- app/app/src/main/java/com/example/myapplication/monitordevices/BLEController.java
+++ app/app/src/main/java/com/example/myapplication/monitordevices/BLEController.java
@@ -19,6 +19,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -28,7 +29,11 @@ import androidx.core.content.ContextCompat;
 import java.util.UUID;
 
 
+/**
+ * TODO [priority: 1/5, difficulty: 1/5] - switch from System.out.println calls to android.util.Log
+ */
 public class BLEController {
+    static String TAG = "BLEController";
     private BluetoothAdapter bluetoothAdapter;
     //private BLEDeviceListAdapter bleDeviceListAdapter = new BLEDeviceListAdapter();
     private boolean scanning = false;
@@ -149,6 +154,23 @@ public class BLEController {
 
     private static final String SAU_REGISTRATION_SERVICE_CHARACTERISTIC_NOTIFIER_UUID_NETWORK_STATE = "dd0a6b3f-86d8-68a9-4141-3ed99c4c2ac7";
 
+
+    private BluetoothGattService sauRegistrationService;
+
+    private BluetoothGattCharacteristic userIdCharacteristic;
+    private BluetoothGattCharacteristic wifiSsidCharacteristic;
+    private BluetoothGattCharacteristic wifiPskCharacteristic;
+    private BluetoothGattCharacteristic networkStateNotificationCharacteristic;
+
+    /*
+        CHARACTERISTICS WRITE FULFILLMENT STATES
+        0 - no characteristics written
+        1 - user id written
+        2 - wifi ssid written
+        3 - wifi psk written
+     */
+    private int state = 0;
+
     private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
         @Override
         public void onCharacteristicChanged(@NonNull BluetoothGatt gatt, @NonNull BluetoothGattCharacteristic characteristic, @NonNull byte[] value) {
@@ -158,57 +180,138 @@ public class BLEController {
         }
 
         @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            if (newState == BluetoothGatt.STATE_CONNECTED) {
-                System.out.println("CONNECTED TO GATT SERVER");
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            System.out.println("onCharacteristicWrite: characteristic uuid: " + characteristic.getUuid() + ", status: " + status);
 
-                BluetoothGattService sauRegistrationService = gatt.getService(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_UUID));
+            switch (state) {
+                case 0:
+                    if (status == BluetoothGatt.GATT_SUCCESS && characteristic.getUuid().equals(UUID.fromString(SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_USER_ID))) {
+                        state = 1;
+                        System.out.println("Writing wifiSsidCharacteristic...");
+                        if (writeCharacteristicCompat(gatt, wifiSsidCharacteristic, BuildConfig.TEST_HARDCODED_WIFI_SSID.getBytes())) {
+                            System.out.println("Wrote wifiSsidCharacteristic, waiting for onCharacteristicWrite to be called");
+                        } else {
+                            System.out.println("ERROR - failed to write wifiSsidCharacteristic"); // TODO handle this
+                        }
+                    } else {
+                        Log.e(TAG, "onCharacteristicWrite for user id - status: " + status);
+                    }
+                    break;
+                case 1:
+                    if (status == BluetoothGatt.GATT_SUCCESS && characteristic.getUuid().equals(UUID.fromString(SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_WIFI_SSID))) {
+                        state = 2;
+                        System.out.println("Writing wifiPskCharacteristic...");
+                        if (writeCharacteristicCompat(gatt, wifiPskCharacteristic, BuildConfig.TEST_HARDCODED_WIFI_PSK.getBytes())) {
+                            System.out.println("Wrote wifiPskCharacteristic, waiting for onCharacteristicWrite to be called");
+                        } else {
+                            System.out.println("ERROR - failed to write wifiPskCharacteristic"); // TODO handle this
+                        }
+                    }
+                    break;
+                case 2:
+                    if (status == BluetoothGatt.GATT_SUCCESS && characteristic.getUuid().equals(UUID.fromString(SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_WIFI_PSK))) {
+                        state = 3;
+                        System.out.println("All required characteristics were written and their onCharacteristicWrite were called with status 0");
+                    }
+                    break;
+                case 3:
+                    throw new RuntimeException("Should not enter onCharacteristicWrite in state 3 (after wifi psk characteristic onCharacteristicWrite was already called with status 0)");
+                default:
+                    throw new RuntimeException("Invalid state: " + state);
 
-                BluetoothGattCharacteristic wifiSsidCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_WIFI_SSID));
-                BluetoothGattCharacteristic wifiPskCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_WIFI_PSK));
-                BluetoothGattCharacteristic userIdCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_USER_ID));
+            }
+        }
 
-                BluetoothGattCharacteristic networkStateNotificationCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_NOTIFIER_UUID_NETWORK_STATE));
+        private boolean writeCharacteristicCompat(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, byte[] value) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    return BluetoothStatusCodes.SUCCESS == gatt.writeCharacteristic(characteristic, value, BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+                } else {
+                    // Backport using deprecated version of writeCharacteristic
+                    characteristic.setValue(value);
+                    return gatt.writeCharacteristic(characteristic);
+                }
+            } catch (SecurityException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                System.out.println("GATT services discovered.");
+
+                sauRegistrationService = gatt.getService(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_UUID));
+
+                wifiSsidCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_WIFI_SSID));
+                wifiPskCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_WIFI_PSK));
+                userIdCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_UUID_USER_ID));
+
+                networkStateNotificationCharacteristic = sauRegistrationService.getCharacteristic(UUID.fromString(BLEController.SAU_REGISTRATION_SERVICE_CHARACTERISTIC_NOTIFIER_UUID_NETWORK_STATE));
 
                 try {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                         System.out.println("SDK >= 33");
 
-                        System.out.println("Enabling notifications for networkStateNotificationCharacteristic...");
-                        if (gatt.setCharacteristicNotification(networkStateNotificationCharacteristic, true)) {
-                            System.out.println("Enabled GATT notifications for networkStateNotificationCharacteristic");
+                    } else {
+                        System.out.println("SDK < 33");
+                        //throw new RuntimeException("Unsupported SDK version (< 33) - possible backport using deprecated version of writeCharacteristic");
+                    }
 
-                            System.out.println("Writing wifiSsidCharacteristic...");
-                            if (BluetoothStatusCodes.SUCCESS == gatt.writeCharacteristic(userIdCharacteristic, "bbbbbbbbaaaaaaaa".getBytes(), BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)) {
-                                System.out.println("Wrote userIdCharacteristic");
-                                System.out.println("Writing wifiSsidCharacteristic...");
-                                if (BluetoothStatusCodes.SUCCESS == gatt.writeCharacteristic(wifiSsidCharacteristic, BuildConfig.TEST_HARDCODED_WIFI_SSID.getBytes(), BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)) {
-                                    System.out.println("Wrote wifiSsidCharacteristic");
-                                    System.out.println("Writing wifiPskCharacteristic...");
-                                    if (BluetoothStatusCodes.SUCCESS == gatt.writeCharacteristic(wifiPskCharacteristic, BuildConfig.TEST_HARDCODED_WIFI_PSK.getBytes(), BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)) {
-                                        System.out.println("Wrote wifiPskCharacteristic");
-                                    }
-                                } else {
-                                    System.out.println("ERROR - failed to write wifiSsidCharacteristic");
-                                }
-                            } else {
-                                System.out.println("ERROR - failed to write userIdCharacteristic");
-                            }
+                    System.out.println("Enabling notifications for networkStateNotificationCharacteristic...");
+                    if (gatt.setCharacteristicNotification(networkStateNotificationCharacteristic, true)) {
+                        System.out.println("Enabled GATT notifications for networkStateNotificationCharacteristic");
 
+
+
+                        System.out.println("Writing userIdCharacteristic...");
+                        if (this.writeCharacteristicCompat(gatt, userIdCharacteristic, "bbbbbbbbaaaaaaaa".getBytes())) {
+                            System.out.println("Wrote userIdCharacteristic, waiting for onCharacteristicWrite to be called");
+
+//                            //System.out.println("The wifi ssid and psk are: " + BuildConfig.TEST_HARDCODED_WIFI_SSID + ", " + BuildConfig.TEST_HARDCODED_WIFI_PSK);
+//                            System.out.println("Writing wifiSsidCharacteristic...");
+//                            if (this.writeCharacteristicCompat(gatt, wifiSsidCharacteristic, BuildConfig.TEST_HARDCODED_WIFI_SSID.getBytes())) {
+//                                System.out.println("Wrote wifiSsidCharacteristic");
+//                                System.out.println("Writing wifiPskCharacteristic...");
+//                                if (this.writeCharacteristicCompat(gatt, wifiPskCharacteristic, BuildConfig.TEST_HARDCODED_WIFI_PSK.getBytes())) {
+//                                    System.out.println("Wrote wifiPskCharacteristic");
+//                                }
+//                            } else {
+//                                System.out.println("ERROR - failed to write wifiSsidCharacteristic");
+//                            }
                         } else {
-                            System.out.println("ERROR - failed to enable GATT notifications for networkStateNotificationCharacteristic");
+                            System.out.println("ERROR - failed to write userIdCharacteristic"); // TODO handle this
                         }
 
                     } else {
-                        System.out.println("SDK < 33");
-                        throw new RuntimeException("Unsupported SDK version (< 33) - possible backport using deprecated version of writeCharacteristic");
+                        System.out.println("ERROR - failed to enable GATT notifications for networkStateNotificationCharacteristic");
                     }
 
                 } catch (SecurityException e) {
                     throw new RuntimeException(e);
                 }
 
+            } else {
+                Log.w(TAG, "onServicesDiscovered - status: " + status);
+            }
+        }
 
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                System.out.println("CONNECTED TO GATT SERVER");
+                try {
+                    System.out.println("Starting remote GATT service discovery...");
+                    if(gatt.discoverServices()) {
+                        System.out.println("Started remote GATT service discovery");
+                    } else {
+                        Log.e(TAG, "ERROR - failed to start remote GATT service discovery");
+                    }
+                } catch (SecurityException e) {
+                    throw new RuntimeException(e);
+                }
             } else {
                 System.out.println("DISCONNECTED FROM GATT SERVER");
             }
